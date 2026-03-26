854d0ffabc5d83aba646c3a0c63848b03c5e8b50
Roland <99310756+rolandarchie93@users.noreply.github.com>
2026-03-25 19:41:29 -0700

    fix(security): Implement secure password handling.

--- scripts/New-IAMUser.ps1
+++ scripts/New-IAMUser.ps1
@@ -1,7 +1,7 @@
 # New-IAMUser.ps1
 # Automates provisioning of new users in Active Directory
 
-Start-Trancript -Path "C:\logs\IAM-Toolkit.log" -Append
+Start-Transcript -Path "C:\logs\IAM-Toolkit.log" -Append
 
 param(
     [Parameter(Mandatory=$true)]
@@ -11,7 +11,7 @@ param(
     [string]$LastName,
 
     [Parameter(Mandatory=$true)]
-    [string]$Department
+    [string]$Department,
 
     [Parameter(Mandatory=$true)]
     [SecureString]$Password
@@ -19,45 +19,43 @@ param(
 
 try {
 
-# Generate username
-$Username = ($FirstName.Substring(0,1) + $LastName).ToLower()
+    # Generate username
+    $Username = ($FirstName.Substring(0,1) + $LastName).ToLower()
 
-# Create User Principal Name
-$UPN = "$Username@iamlab.local"
+    # Create User Principal Name
+    $UPN = "$Username@iamlab.local"
 
+    # Create Active Directory user
+    New-ADUser `
+        -Name "$FirstName $LastName" `
+        -GivenName $FirstName `
+        -Surname $LastName `
+        -SamAccountName $Username `
+        -UserPrincipalName $UPN `
+        -AccountPassword $Password `
+        -Enabled $true `
+        -Path "OU=$Department,OU=Departments,DC=iamlab,DC=local"
 
-# Create Active Directory user
-New-ADUser `
-    -Name "$FirstName $LastName" `
-    -GivenName $FirstName `
-    -Surname $LastName `
-    -SamAccountName $Username `
-    -UserPrincipalName $UPN `
-    -AccountPassword $Password `
-    -Enabled $true `
-    -Path "OU=$Department,OU=Departments,DC=iamlab,DC=local"
+    # Assign RBAC group
+    switch ($Department) {
+        "Finance"   { $GroupName = "FIN_Modify" }
+        "HR"        { $GroupName = "HR_Modify" }
+        "IT"        { $GroupName = "IT_Modify" }
+        "Sales"     { $GroupName = "SAL_Modify" }
+        "Executive" { $GroupName = "EXEC_Modify" }
+    }
 
-  # Assign RBAC group
-switch ($Department) {
-    "Finance" { $GroupName = "FIN_Modify" }
-    "HR"      { $GroupName = "HR_Modify" }
-    "IT"      { $GroupName = "IT_Modify" }
-    "Sales"   { $GroupName = "SAL_Modify" }
-    "Executive" { $GroupName = "EXEC_Modify" }
-}
+    Add-ADGroupMember -Identity $GroupName -Members $Username 
 
-Add-ADGroupMember -Identity $GroupName -Members $Username 
-
-# Display success message
-Write-Host "User $FirstName $LastName created successfully." -ForegroundColor Green
-Write-Host "Username: $Username"
-Write-Host "Department: $Department"
-Write-Host "RBAC Group Assigned: $GroupName"
+    # Display success message
+    Write-Host "User $FirstName $LastName created successfully." -ForegroundColor Green
+    Write-Host "Username: $Username"
+    Write-Host "Department: $Department"
+    Write-Host "RBAC Group Assigned: $GroupName"
 }
 
 catch {
     Write-Host "ERROR: $($_.Exception.Message)" -ForegroundColor Red
 }
-
 
 Stop-Transcript
